cdeff04b41e74c7e63be086735b664d8252fd68b
Max Kulik <pearelectronics@live.com>
2023-07-29 21:53:12 -0500

    Added Installer
    Basic auto-installer added. Still working to add configuration options.

---
+++ installer.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#Let's stop the script if anything errors out. 
+echo Script will stop if error detected. 
+set -o errexit
+
+#Let's start by making sure we are up to date
+echo Making sure system is up-to-date.
+sudo apt update && sudo apt upgrade
+
+#make sure wget is installed 
+sudo apt install wget
+
+#Install the Transmission Killswitch script
+mkdir /etc/TransmissionKillswitch
+cd /etc/TransmissionKillswitch/
+wget https://raw.githubusercontent.com/Glitch3dPenguin/Transmission-KillSwitch/main/kill-switch.sh 
+cd 
+
+#Set up the auto run script
+echo Installing Killswitch Service to /etc/systemd/system/killswitch.service
+cat <<EOF > /etc/systemd/system/killswitch.service
+[Unit]
+Description=Manual Transmission Killswitch
+After=openvpn.service
+
+[Service]
+Type=simple
+ExecStartPre=/usr/bin/sleep 30
+ExecStart=/root/kill-switch.sh
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+#Enabled the service for systemctl
+sudo systemctl enable killswitch
